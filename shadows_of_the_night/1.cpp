d22376a835bd3a8835c98eb5ca31f238fd0b6e18
Shinsuke KITA <shinsukekita@Shins-MacBook-Air.local>
2023-02-05 16:40:44 +0900

    230205

---
+++ shadows_of_the_night/1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int w; // width of the building.
+    int h; // height of the building.
+    cin >> w >> h; cin.ignore();
+    int n; // maximum number of turns before game over.
+    cin >> n; cin.ignore();
+    int x0;
+    int y0;
+    cin >> x0 >> y0; cin.ignore();
+
+    // game loop
+    while (1) {
+        string bomb_dir; // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+        cin >> bomb_dir; cin.ignore();
+
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+
+        // the location of the next window Batman should jump to.
+        cout << "0 0" << endl;
+    }
+}
